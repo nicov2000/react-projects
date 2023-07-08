a7837e623c05251de5402a1d39f8bf9b33ba6e98
nicov2000 <nicolasvarg00@gmail.com>
2023-07-08 01:13:58 -0300

    updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+root.render(<App />)
